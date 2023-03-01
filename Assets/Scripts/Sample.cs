a2bedae5fb182e08a161d349caa999b73263f99a
jihun-heo <karais89@gmail.com>
2023-03-01 23:01:29 +0900

    main update sample

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Sample : MonoBehaviour
@@ -13,6 +11,6 @@ public class Sample : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("main update");
     }
 }
