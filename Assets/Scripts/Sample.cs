181707f1ba3af3a688f1c6d260acd4068eabc2b0
jihun-heo <karais89@gmail.com>
2023-03-01 23:01:10 +0900

    Sample Script modify

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -5,7 +5,7 @@ public class Sample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("feature-1");
     }
 
     // Update is called once per frame
