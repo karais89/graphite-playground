69e0976711cc2a1cf6feac3392dfa04741fe78aa
jihun-heo <karais89@gmail.com>
2023-03-01 23:35:55 +0900

    추가 수정

--- Assets/Scripts/Sample3.cs
+++ Assets/Scripts/Sample3.cs
@@ -6,6 +6,7 @@ public class Sample3 : MonoBehaviour
     void Start()
     {
         Debug.Log("Sample3에 대한 기능 테스트");
+        Debug.Log("Sample3에 대한 기능 테스트 추가 수정"); 
     }
 
     // Update is called once per frame
